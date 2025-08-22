13a96c2f9c7842f9dd529d13ab33a7329ba76143
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-22 18:23:31 +0300

    KS-1-3 task completed

--- src/main/kotlin/lesson1/lesson1_Task3.kt
+++ src/main/kotlin/lesson1/lesson1_Task3.kt
@@ -1,8 +1,15 @@
 package org.example.lesson1
 
+import kotlin.math.min
+
 fun main() {
-
-    val quantityOfOrders : Int = 75
-    val gratitudeMessage : String = "Спасибо за заказ! Вам будут начислены бонусные баллы."
-
+    val year: Int = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+    hour = 10
+    minute = 55
+    print("$hour:$minute")
 }
